a749181471a83112beda2d437744575f699130a7
codescy <chaudharysneh02@gmail.com>
2026-09-21 13:50:24 +0530

    Terms and condition

--- resources/views/crm/estimates/partials/terms-conditions-form.blade.php
+++ resources/views/crm/estimates/partials/terms-conditions-form.blade.php
@@ -1,3 +1,9 @@
+@php
+    $defaultTermsConditions = "- GST Charges - As applicable at the time of billing.\n"
+        . "- Meter charges extra as actual as per GEB (for 3phase consumer)\n"
+        . "- Any other work; out of the scope or change in given planning will be handled through mutual agreement.";
+    $termsConditionsValue = isset($estimate) ? $estimate->terms_conditions : $defaultTermsConditions;
+@endphp
 <div class="mt-3">
             <label for="attach_file" class="form-label fw-semibold">Attach File to Estimate</label>
             <input type="file" name="attach_file" id="attach_file"
@@ -21,7 +27,7 @@
     <textarea name="terms_conditions" id="terms_conditions" rows="9" maxlength="10000"
         class="form-control @error('terms_conditions') is-invalid @enderror"
         style="min-height: 140px;height:175px;"
-        placeholder="Enter the terms and conditions for this estimate...">{{ old('terms_conditions', $estimate->terms_conditions ?? '') }}</textarea>
+        placeholder="Enter the terms and conditions for this estimate...">{{ old('terms_conditions', $termsConditionsValue) }}</textarea>
     <div class="invalid-feedback" id="terms_conditions-error">
         @error('terms_conditions')
             {{ $message }}
